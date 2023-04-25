a0f3d3aebb6753db75a5edcaba3d19f96b76e130
amarjyothi98 <amarjyothi526@gmail.com>
2023-04-25 07:38:11 +0530

    home page set up and routing completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import "./index.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./components/Signin";
 
 function App() {
   return (
     <div>
-      
+      <h1 className="text-center text-3xl font-bold">
+        Firebase and Auth Context
+      </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signin/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
